13e1f6e81d885d92f717903d2eeb8c13b4668fd2
victor_moreira <victormoreira@vivaldi.net>
2024-05-17 18:28:31 -0300

    DatabaseManager iniciado; bug: o programa não roda

--- src/view/Care5w2h.java
+++ src/view/Care5w2h.java
@@ -89,6 +89,7 @@ public class Care5w2h extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
